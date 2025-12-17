ee42055de4753000fc65b75da9d723f0e5f0eb82
Bruno Caneco <bruno@dmpstats.co.uk>
2025-12-17 22:48:57 +0000

    Update RFunction.R

--- RFunction.R
+++ RFunction.R
@@ -164,7 +164,7 @@ rFunction = function(data,
     api_base_url = api_base_url,
     token = api_token, 
     unclust_min_date = min(data[[tm_id_col]]) - lubridate::days(lookback), 
-    page_size = 1500
+    page_size = 2000
   )
   
   
